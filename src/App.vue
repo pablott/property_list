3c4384ad7f6d35fe03b2a98c4f5c7cd3a4f4b7c3
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-10-20 23:01:34 +0100

    Fetch JSON data from API

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,22 @@ export default {
   name: 'app',
   components: {
     PropertyList
+  },
+  data () {
+    return {
+      city: null,
+      list: null,
+    }
+  },
+  mounted () {
+    axios
+      .get('https://gist.githubusercontent.com/ruimendesM/bf8d095f2e92da94938810b8a8187c21/raw/70b112f88e803bf0f101f2c823a186f3d076d9e6/properties.json')
+      .then(response => {
+        console.log(response);
+        this.city = response.data.location.city.name;
+        this.list = response.data.properties;
+        console.log(this.city);
+      })
   }
 }
 </script>
