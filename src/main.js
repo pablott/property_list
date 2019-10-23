b0070652cffbf65b0a6ccf6ca68652cdc08177ac
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2019-10-23 05:23:10 +0100

    Added vue-material

--- src/main.js
+++ src/main.js
@@ -2,8 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+// Vue Material components and CSS
+import { MdList, MdIcon, MdButton, MdDivider, MdToolbar, MdSubheader} from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/theme/default.css'
+// Vue Material
+Vue.use(MdList)
+Vue.use(MdIcon)
+Vue.use(MdButton)
+Vue.use(MdDivider)
+Vue.use(MdToolbar)
+Vue.use(MdSubheader)
+
+/*
+// Import all
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/theme/default.css'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+*/
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
